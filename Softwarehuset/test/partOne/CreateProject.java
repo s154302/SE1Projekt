318968f728b7a31f3924a2fbd1ff03778690590d
Emilie Isabella Dahl <s153762@student.dtu.dk>
2016-04-11 16:03:38 +0200

    new version of test

--- Softwarehuset/test/partOne/CreateProject.java
+++ Softwarehuset/test/partOne/CreateProject.java
@@ -7,8 +7,11 @@ import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.Test;
-
+//Softwarehuset har faet en opgave fra DTU, som skal være færdig om d.
+//10. Marts 2018. Hertil skal der oprettes et projekt ved navn, 'DTU - campusNet',
+//og det skal starte omkring et ar fra afleveringsdatoen.
 public class CreateProject {
 
 	@Test
@@ -18,12 +21,20 @@ public class CreateProject {
 		ArrayList<Employee> employeeList = new ArrayList<Employee>();
 		employeeList.add(e1);
 		
+		//list of nonprojects
 		ArrayList<NonProjectActivity> nonProjectActivityList = new ArrayList<NonProjectActivity>();
 		
+		//list of projects
 		ArrayList<Project> projectList = new ArrayList<Project>();
 		
 		Model model = new Model(employeeList, nonProjectActivityList, projectList);
 		
+		assertEquals(model.projectList, null);
+
+		
+		model.createProject("DTU - campusNet", dateStart, dateFinish);
+		
+		Assert.assertNotNull(model.projectList);
 		
 	}
 
