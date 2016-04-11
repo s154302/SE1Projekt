a506d30dc18c0cbbb0dd563e83e67e43349749e5
Emilie Isabella Dahl <s153762@student.dtu.dk>
2016-04-11 15:33:49 +0200

    test class

---
+++ Softwarehuset/test/partOne/CreateProject.java
@@ -0,0 +1,17 @@
+package partOne;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class CreateProject {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
