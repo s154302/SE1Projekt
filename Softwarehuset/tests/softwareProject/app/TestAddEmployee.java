853cefcd00f0aa615affe3b697ccbeada9d47ffb
Alexander Kristian Armstrong <alexander.armstrong@hotmail.com>
2016-04-11 15:48:40 +0200

    metadata
    metadata

---
+++ Softwarehuset/tests/softwareProject/app/TestAddEmployee.java
@@ -0,0 +1,16 @@
+package softwareProject.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAddEmployee {
+
+	// Tests the scenario where an employee is added to the employee list of an activity.
+	
+	@Test
+	public void testAddEmployee() {
+		assertFalse()
+	}
+
+}
