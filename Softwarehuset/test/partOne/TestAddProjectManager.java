706f1ec3241ed934d6e2277904b864598364491f
Emilie Isabella Dahl <s153762@student.dtu.dk>
2016-04-25 15:56:34 +0200

    usecases in test classes

---
+++ Softwarehuset/test/partOne/TestAddProjectManager.java
@@ -0,0 +1,14 @@
+package partOne;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAddProjectManager {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
