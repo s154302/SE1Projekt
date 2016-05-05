e36a7f0e8a49cfcb2a55ff49ab2145592e5b3603
tk7000 <s154219@student.dtu.dk>
2016-05-05 17:45:05 +0200

    Skrevet test til projektReport

--- Softwarehuset/test/test/TestCreateProjectRaport.java
+++ Softwarehuset/test/test/TestCreateProjectRaport.java
@@ -2,13 +2,25 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
 import org.junit.Test;
+
+import code.Model;
+import code.OperationNotAllowedException;
 
 public class TestCreateProjectRaport {
 
+	Model model = new Model();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws OperationNotAllowedException, FileNotFoundException, UnsupportedEncodingException {
+		model.createProject(null, null, 0, 0, 0, 0, 0, 0);
+		model.reportProject(model.projectList.get(0));
+		File file = new File("Report for "+model.projectList.get(0).getName());
+		assertTrue(file.exists());
 	}
 
 }
