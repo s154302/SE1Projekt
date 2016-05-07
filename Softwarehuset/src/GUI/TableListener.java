95c8cae01e4ce1e16df01bbfc7fcb1fcc5042894
Emilie Isabella Dahl <s153762@student.dtu.dk>
2016-05-07 03:44:30 +0200

    small changes
    sorry guys, men det tableListener fucker med mig!

--- Softwarehuset/src/GUI/TableListener.java
+++ Softwarehuset/src/GUI/TableListener.java
@@ -1,62 +1,3 @@
-//<<<<<<< HEAD
-//package GUI;
-//
-//import javax.swing.JTable;
-//import javax.swing.event.ListSelectionEvent;
-//import javax.swing.event.ListSelectionListener;
-//
-//import code.Activity;
-//import code.Model;
-//import code.Project;
-//
-//public class TableListener implements ListSelectionListener{
-//	private Model model;
-//	private Project p;
-//	private Frame f;
-//	private JTable table;
-//	private Project project;
-//
-//
-//	public TableListener(Model model, Frame f, JTable table){
-//		this.model = model;
-//		this.f = f;
-//		this.table = table;
-//	}
-//	
-//	public void valueChanged(ListSelectionEvent event) {
-//        if (table.getSelectedRow() < model.projectList().size() && table.getSelectedRow()>-1 
-//        		&& table.getValueAt(0,0).equals(model.projectList.get(0).getSerialNumber())) {
-//
-//            f.getProjectPanel().setActivityPanel(table.getValueAt(table.getSelectedRow(), 0).toString());
-//            f.update();
-//            
-//        } else if( table.getSelectedRow()>-1) {
-//        	ActivityFrame activityFrame = new ActivityFrame(model, f);
-//        	
-//        
-//        } else{
-//        	System.out.println("nej");
-//        }
-//        	
-//        }
-//    public Project getProject(){
-//   	 if (table.getSelectedRow() < model.projectList().size() && table.getSelectedRow()>-1 && table.getValueAt(0,0).equals(model.projectList.get(0).getSerialNumber())) {
-//         project =   model.searchProject(table.getValueAt(table.getSelectedRow(), 0).toString());
-//   		 return project;
-//            }
-//   	 return null;
-//   	 }
-//    
-//    public Activity getActivity(){
-//      	 if (table.getSelectedRow()>-1 && !table.getValueAt(0,0).equals(model.projectList.get(0).getSerialNumber())) {
-//               return project.searchActivity(table.getValueAt(table.getSelectedRow(), 0).toString());
-//               }
-//      	 System.out.println("TableListener error");
-//      	 return null;
-//      	 }
-//
-//}
-//=======
 package GUI;
 
 import javax.swing.JTable;
@@ -69,59 +10,66 @@ import code.Project;
 
 public class TableListener implements ListSelectionListener{
 	private Model model;
-	private Project p;
 	private Frame f;
 	private JTable tableProject,tableActivity;
 	private Project project;
 	private Activity activity;
 	private ActivityFrame activityFrame;
-
+	private boolean tableActivitySelected;
 
 	public TableListener(Model model, Frame f, JTable table){
 		this.model = model;
 		this.f = f;
 		this.tableProject = table;
 		this.tableActivity = null;
+		project = null;
+		activity = null;
+		tableActivitySelected = false;
 	}
 	
 	public void valueChanged(ListSelectionEvent event) {
-    	if(tableActivity != null) {
+    	if(tableActivitySelected) {
     		if(tableActivity.getSelectedRow()>-1){
-    			
-    	activity = project.searchActivity(tableActivity.getValueAt(tableActivity.getSelectedRow(), 0).toString());
-    	activityFrame = new ActivityFrame(model, f, activity);
+    			activity = project.searchActivity(tableActivity.getValueAt(tableActivity.getSelectedRow(), 0).toString());
+    			activityFrame = new ActivityFrame(model, f, activity);
     		}
-            
-        } else if (tableProject.getSelectedRow() < model.projectList().size() && tableProject.getSelectedRow()>-1 
-        		&& tableProject.getValueAt(0,0).equals(model.projectList.get(0).getSerialNumber())) {
-        	activity = null;
-        	project =   model.searchProject(tableProject.getValueAt(tableProject.getSelectedRow(), 0).toString());
+        } else if (tableProjectSelected()) {
+        	project = model.searchProject(tableProject.getValueAt(tableProject.getSelectedRow(), 0).toString());
          f.getProjectPanel().setActivityPanel(project);
             f.update();
-
+            tableActivitySelected = false;
         } else{
         	System.out.println("tableListener");
         }
         	
-        }
+   }
+
+	private boolean tableProjectSelected() {
+		return tableProject.getSelectedRow() < model.projectList().size() && tableProject.getSelectedRow()>-1 
+        		&& tableProject.getValueAt(0,0).equals(model.projectList.get(0).getSerialNumber());
+	}
 	
     public Project getProject(){
-   	 if (tableProject.getSelectedRow() < model.projectList().size() && tableProject.getSelectedRow()>-1 && tableProject.getValueAt(0,0).equals(model.projectList.get(0).getSerialNumber())) {
+   	 if (tableProjectSelected()) {
          project =   model.searchProject(tableProject.getValueAt(tableProject.getSelectedRow(), 0).toString());
-   		 return project;
+         tableActivitySelected = false;
+         return project;
             }
    	 return null;
    	 }
     
     public Activity getActivity(){
       	 if (tableActivity != null) {
+      		 tableActivitySelected = true;
       		 return activity;
+      		 
          }
       	 return null;
     }
     
     public void setTable(JTable table){
     	this.tableActivity=table;
+    	tableActivitySelected = true;
     }
     
     public ActivityFrame getActivityFrame(){
