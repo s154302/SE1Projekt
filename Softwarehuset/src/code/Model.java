31cbd3c5582914e1e7358ea8dc8afef1ec9b7f18
tk7000 <s154219@student.dtu.dk>
2016-05-09 02:03:34 +0200

    Små rettelser

--- Softwarehuset/src/code/Model.java
+++ Softwarehuset/src/code/Model.java
@@ -21,14 +21,11 @@ public class Model {
 		this.projectList = new ArrayList<Project>();
 		this.nonProjectActivityList = new ArrayList<NonProjectActivity>();
 		this.employeeList = new ArrayList<Employee>();
-		
 
 		this.nonProjectActivityList.add(new NonProjectActivity("Courses"));
-		this.nonProjectActivityList.add(new NonProjectActivity("Sick"));
-		this.nonProjectActivityList.add(new NonProjectActivity("Vacation"));
-		this.nonProjectActivityList.add(new NonProjectActivity("Other"));
-		
-		
+		this.nonProjectActivityList.add( new NonProjectActivity("Sick"));
+		this.nonProjectActivityList.add( new NonProjectActivity("Vacation"));
+		this.nonProjectActivityList.add( new NonProjectActivity("Other"));
 		
 	}
 
@@ -95,15 +92,6 @@ public class Model {
 		return counterString;
 	}
 
-	// Creates a new Non-project activity and adds it to its list.
-	public void createNonProjectActivity() {
-		// Create a NonProjectActivity.
-	}
-
-	// Deletes a project.
-//	public void removeProject(Project project) {
-//		this.projectList.remove(project);
-//	}
 
 	// Deletes an employee.
 	public void removeEmployee(Employee employee) {
